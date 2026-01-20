c3b8cae04e9641f0979a3d14a509e6ae9350c818
Ishvari Prasad Saini <sainiiswari@gmail.com>
2026-01-20 10:34:44 +0530

    latest em

--- src/utils/mailer.util.js
+++ src/utils/mailer.util.js
@@ -4,7 +4,8 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.EMAIL_PORT), // 🔥 IMPORTANT
-  secure: false, // true only for 465
+  // secure: false,
+  secure: true, // true only for 465
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
