01bd9ffec6651efe2106e1b15c8c7afdebbb5860
Ishvari Prasad Saini <sainiiswari@gmail.com>
2026-02-22 00:46:35 +0530

    local EC2

--- src/server.js
+++ src/server.js
@@ -17,10 +17,9 @@ const PORT = process.env.PORT || 5000;
     console.log(" DB synced");
 
     // Start server
-    app.listen(PORT, () => {
-      console.log(` Server running on http://localhost:${PORT}`);
-    });
-
+    app.listen(PORT, "0.0.0.0", () => {
+  console.log(` Server running on port ${PORT}`);
+});
   } catch (error) {
     console.error(" DB connection failed:", error.message);
   }
