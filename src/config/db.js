1e0f5c0402747c32aab54538048362ca293bf379
Ishvari Prasad Saini <sainiiswari@gmail.com>
2026-02-21 17:56:48 +0530

    db fix

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,18 @@
+require("dotenv").config();
 const { Sequelize } = require("sequelize");
+
+console.log(" USING LOCAL EC2 DATABASE");
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASS,   // 🔥 .env me DB_PASS use karenge
+  process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
+    port: process.env.DB_PORT,
     dialect: "postgres",
     logging: false,
   }
 );
-
-console.log("🌍 USING LOCAL EC2 DATABASE");
 
 module.exports = sequelize;
