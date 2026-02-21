76094febbe56c62ac2c1eced4c7d7b78de16e6de
Ishvari Prasad Saini <sainiiswari@gmail.com>
2026-02-21 16:46:41 +0530

    Fixed SSL issue for EC2 local postgres

--- src/config/db.js
+++ src/config/db.js
@@ -1,59 +1,17 @@
-// const { Sequelize } = require("sequelize");
-
-// const isLiveDB = process.env.DB_MODE === "live";
-
-// let sequelize;
-
-// if (isLiveDB) {
-//   sequelize = new Sequelize(process.env.LIVE_DATABASE_URL, {
-//     dialect: "postgres",
-//     logging: false,
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false,
-//       },
-//     },
-//   });
-
-//   console.log(" USING LIVE RENDER DB");
-// } else {
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASS,
-//     {
-//       host: process.env.DB_HOST,
-//       port: process.env.DB_PORT,
-//       dialect: "yatra_db_user",
-//       logging: false,
-//     }
-//   );
-
-//   console.log(" USING LOCAL DB");
-// }
-
-// module.exports = sequelize;
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,     // yatra_db
-  process.env.DB_USER,     // yatra_db_user
-  process.env.DB_PASS,     // render password
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,   // 🔥 .env me DB_PASS use karenge
   {
-    host: process.env.DB_HOST, // 35.227.164.209 OR render hostname
+    host: process.env.DB_HOST,
     port: process.env.DB_PORT || 5432,
     dialect: "postgres",
     logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,            // 🔥 Render needs SSL
-        rejectUnauthorized: false // 🔥 for local
-      },
-    },
   }
 );
 
-console.log("🌍 USING SINGLE LIVE DATABASE");
+console.log("🌍 USING LOCAL EC2 DATABASE");
 
 module.exports = sequelize;
